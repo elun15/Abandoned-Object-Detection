348a2c2afeaa301263238ef2b827940ca2fa46bd
Juan C. SanMiguel <juancarlos.sanmiguel@uam.es>
2017-10-30 15:39:43 +0100

    removed unneded files

--- src/BLOBS/CompareBlobs.h
+++
@@ -1,7 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include "BasicBlob.h"
-
-
-bool CompareBlobs(cvBlob blob, cv::Rect rect);
-
--- src/Utils/solape.h
+++
@@ -1,4 +0,0 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
-
-double solape(cv::Rect hogRect, cv::Rect bloblistRect);
